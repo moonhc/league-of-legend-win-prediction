4196a9adb09a4b47f41b47761e17a8f6db3fdfc0
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2019-05-12 23:03:17 +0900

    Create Champion.js

---
+++ client/src/components/Champion.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../static/assets/Champion.css';
+import PropTypes from 'prop-types';
+
+function ChampionImage({image}){
+  return(
+    <img className='champImage' src={image} />
+  )
+}
+class Champion extends Component {
+  static propTypes = {
+    title : PropTypes.string,
+    image : PropTypes.string
+  }
+
+  render() {
+    return (
+      <div className="Champion">
+        <ChampionImage image={this.props.image} />
+      </div>
+    );
+  }
+}
+export default Champion;
