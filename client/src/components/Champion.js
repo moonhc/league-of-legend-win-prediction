3711de2d9ac9c930a7e3c444a027bb77d4a4c1d7
sssjjjaaa <37531907+SeongJinAhn@users.noreply.github.com>
2019-05-27 17:18:30 +0900

    Champion file changed (Inserted Item)
    Champion file changed (Inserted Item)

--- client/src/components/Champion.js
+++ client/src/components/Champion.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import '../static/assets/Champion.css';
 import PropTypes from 'prop-types';
+import App from '../App'
 
-function ChampionImage({image}){
-  return(
-    <img className='champImage' src={image} />
-  )
+function ChampionImage({image,number,team}){
+  if(team=='1')
+  {
+    return(
+      <img className='teamImage' src={image}
+      onMouseOver={e => (e.currentTarget.src="https://raw.githubusercontent.com/SeongJinAhn/league-of-legend-win-prediction/master/item/"+String(number)+"-1.jpg")}
+      onMouseOut={e => (e.currentTarget.src=image)}
+      />    
+      )
+  }
+  else{
+    return(
+     <img className='champImage' src={image}
+      />
+   )
+  }
 }
 class Champion extends Component {
   static propTypes = {
     title : PropTypes.string,
     image : PropTypes.string
   }
-
   render() {
     return (
       <div className="Champion">
-        <ChampionImage image={this.props.image} />
+        <ChampionImage image={this.props.image} number={this.props.number} team={this.props.team}
+        />
       </div>
     );
   }
